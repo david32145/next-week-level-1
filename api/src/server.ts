c74979957c118ead4c9864c086f727ef37b461d8
david32145 <nascimento32145@gmail.com>
2020-06-03 14:49:40 -0300

    Config routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,11 @@
 import express from "express"
+import routes from "./routes"
 
 const app = express()
 
-app.listen(3333)
+app.use(express.json())
+app.use(routes)
+
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT)
