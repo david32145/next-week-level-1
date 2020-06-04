ec7b8f9d46530af6f9f87c589c04ae0088421d50
david32145 <nascimento32145@gmail.com>
2020-06-03 21:29:21 -0300

    Add faker image url

--- api/src/controllers/PointController.ts
+++ api/src/controllers/PointController.ts
@@ -38,7 +38,7 @@ class PointController {
         logitude, 
         city, 
         uf, 
-        image_url: "image faker"
+        image_url: "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
       }
       const [point_id]: number[] = await trx("points").insert(newPoint)
 
