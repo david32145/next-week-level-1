800aca939d19a67504f4a4189f3419ef74becfac
david32145 <nascimento32145@gmail.com>
2020-06-04 15:29:56 -0300

    Config routes

---
+++ web/src/pages/NewPoint/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles"
+
+const NewPointPage: React.FC = () => {
+  return (
+    <Container>
+      new point page
+    </Container>
+  );
+};
+
+export default NewPointPage;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
 
 import PageHome from "./pages/Home";
+import NewPointPage from "./pages/NewPoint"
+
 
 const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={PageHome} />
+        <Route path="/home" exact component={PageHome} />
+        <Route path="/point/new" exact component={NewPointPage} />
+        <Route path="*" >
+          <Redirect to="/home" />
+        </Route>/
       </Switch>
     </BrowserRouter>
   );
